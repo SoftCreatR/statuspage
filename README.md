dc49ba1e82ffc2d041601b196149a759a261b5d2
Sascha Greuel <sascha@softcreatr.de>
2020-09-28 20:08:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Live status: <!--live status-->
+
+<!--start: status pages-->
+
+<!--end: status pages-->
+
+Icon made from [Icon Fonts](http://www.onlinewebfonts.com/icon) is licensed by CC BY 3.0
---
+++ site/src/routes/_layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+  import config from "../data/config.json";
+  import snarkdown from "snarkdown";
+  export let segment;
+</script>
+
+<style>
+  footer {
+    text-align: center;
+    opacity: 0.75;
+    margin-top: 3rem;
+  }
+</style>
+
+<Nav {segment} />
+
+<main class="container">
+  <slot />
+</main>
+
+<footer>
+  <p><!-- --></p>
+</footer>
